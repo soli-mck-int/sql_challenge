796457c01ab80bb269f189b4b7c098e297dae9bf
solicia <solicia@solicias-mbp.attlocal.net>
2019-12-07 21:56:25 -0600

    make changes to table_Create

--- table_create.sql
+++ table_create.sql
@@ -1,29 +1,24 @@
 create table department(
 	dept_no varchar primary key,
-	dept_name varchar not null
-	);
+	dept_name varchar);
 create table employees(
-	emp_no int primary key,
+	emp_no varchar primary key,
 	birth_date date,
 	first_name varchar,
 	last_name varchar,
 	gender varchar,
 	hire_date date);
 create table dept_emp(
-	emp_no int,
-	dept_no varchar,
+	emp_no varchar references employees (emp_no),
+	dept_no varchar references department (dept_no),
 	from_date date,
 	to_data date,
-	FOREIGN KEY (emp_no)  REFERENCES employees (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES department (dept_no),
 	primary key (emp_no,dept_no));
 create table Dept_manager(
-	dept_no varchar,
-	emp_no int,
+	dept_no varchar references department (dept_no),
+	emp_no varchar references employees (emp_no),
 	from_date date,
 	to_date date,
-	FOREIGN KEY (emp_no)  REFERENCES employees (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES department (dept_no),
 	primary key (emp_no,dept_no));
 create table salaries(
 	emp_no int primary key,
